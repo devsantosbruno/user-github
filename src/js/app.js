e1a447f1d917785d7abaacd781151bc7b689deb5
bruno santos <devbrunosantos@gmail.com>
2022-06-18 21:08:25 -0800

    feat: get user with input and show avatar or user not found

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,16 @@ require('jquery')
 require('bootstrap')
 require('@fortawesome/fontawesome-free')
 import axios from 'axios'
+
+$('#confirmTeste').on('click', function () {
+  let userGithub = $('#userGithub').val()
+  axios
+    .get(`https://api.github.com/users/${userGithub}`)
+    .then((response) => {
+      $('#photUser').prop('src', response.data.avatar_url)
+    })
+    .catch((e) => {
+      console.log(e.message)
+      $('#infos').append(`<h1>User not found</h1>`)
+    })
+})
